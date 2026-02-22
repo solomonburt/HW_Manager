73f1bb94671e1c2a830bea21b01aaa149b9a06e5
solomonburt <sdburt@syr.edu>
2026-02-22 18:48:38 -0500

    Update navigation for homework assignments

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 import streamlit as st
 
-# Setup navigation pages
+# navigation set up
 pg = st.navigation([
     st.Page("HW/HW1.py", title="Homework 1: File Summarizer"),
     st.Page("HW/HW2.py", title="Homework 2: URL Summarizer"),
     st.Page("HW/HW3.py", title="Homework 3: URL Chatbot"),
-    st.Page("HW/HW4.py", title="Homework 4: iSchool Chatbot") # Added HW
-
+    st.Page("HW/HW4.py", title="Homework 4: RAG Chatbot"),
+    st.Page("HW/HW5.py", title="Homework 5: Intelligent Chatbot") # Added for HW5
 ])
 
-# Shared page configuration
 st.set_page_config(page_title="HW Manager", layout="wide")
-
-# App title
 st.title("HW Manager")
 st.write("Use the sidebar to navigate between your assignments.")
 
-# Run navigation
 pg.run()
 
 
