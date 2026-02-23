577e2247a26740f6d4ecc411ea1fa79b859409e5
solomonburt <sdburt@syr.edu>
2026-02-22 19:16:10 -0500

    Refactor navigation setup for homework pages

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import streamlit as st
 
-# navigation set up
-pg = st.navigation([
-    st.Page("HW/HW1.py", title="Homework 1: File Summarizer"),
-    st.Page("HW/HW2.py", title="Homework 2: URL Summarizer"),
-    st.Page("HW/HW3.py", title="Homework 3: URL Chatbot"),
-    st.Page("HW/HW4.py", title="Homework 4: RAG Chatbot"),
-    st.Page("HW/HW5.py", title="Homework 5: Intelligent Chatbot") # Added for HW5
-])
+# define the pages
+hw1 = st.Page("HW/HW1.py", title="Homework 1: File Summarizer")
+hw2 = st.Page("HW/HW2.py", title="Homework 2: URL Summarizer")
+hw3 = st.Page("HW/HW3.py", title="Homework 3: URL Chatbot")
+hw4 = st.Page("HW/HW4.py", title="Homework 4: RAG Chatbot", default=True)
+hw5 = st.Page("HW/HW5.py", title="Homework 5: Intelligent Chatbot") # Added for HW5
 
+# navigation setup
+pg = st.navigation([hw1, hw2, hw3, hw4, hw5])
 st.set_page_config(page_title="HW Manager", layout="wide")
 st.title("HW Manager")
 st.write("Use the sidebar to navigate between your assignments.")
 
 pg.run()
+
+
 
 
