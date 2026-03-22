fd7aa399748b60578a4109f7dedea5892768f366
solomonburt <sdburt@syr.edu>
2026-03-22 10:11:10 -0400

    Add Homework 7: News Chatbot to navigation

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ st.write("Use the sidebar to navigate between your assignments.")
 hw1 = st.Page("HW/HW1.py", title="Homework 1: File Summarizer")
 hw2 = st.Page("HW/HW2.py", title="Homework 2: URL Summarizer")
 hw3 = st.Page("HW/HW3.py", title="Homework 3: URL Chatbot")
-hw4 = st.Page("HW/HW4.py", title="Homework 4: RAG Chatbot", default=True)
-hw5 = st.Page("HW/HW5.py", title="Homework 5: Intelligent Chatbot") # Added for HW5
-
+hw4 = st.Page("HW/HW4.py", title="Homework 4: RAG Chatbot")
+hw5 = st.Page("HW/HW5.py", title="Homework 5: Intelligent Chatbot") 
+hw7 = st.Page("HW/HW7.py", title="Homework 7: News Chatbot", default=True) # Added for HW7
 # navigation setup
-pg = st.navigation([hw1, hw2, hw3, hw4, hw5])
+pg = st.navigation([hw1, hw2, hw3, hw4, hw5, hw7])
 
 
 pg.run()
